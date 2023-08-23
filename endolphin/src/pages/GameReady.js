f5ad1ba25f45bd19a54a6d41e51b16d3acd59497
yunyoung0531 <yunyoung0531@naver.com>
2023-08-24 00:15:18 +0900

    게임 준비상태 UI

--- endolphin/src/pages/GameReady.js
+++ endolphin/src/pages/GameReady.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import 쑥 from './imgs/쑥.png';
 import 일반쓰레기통 from './imgs/일반쓰레기통.png';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function GameReady() {
+
+    let navigate = useNavigate();
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClose = () => setShowModal(false);
+    const handleShow = () => setShowModal(true);
+
     return (
         <>
         <div className="game-main">
             <div className='game-main-writing-1'>쑥오와 함께하는</div>
-            <div className='game-main-writing-2'>분리배출 게임</div>
+            <div className='game-main-writing-2'>분리수거(배출) 게임</div>
         </div>
 
         <div className="game-main-bg">
             <div className="game-main-bg-1" style={{ backgroundImage: `url(${쑥})` }} />
             <div className='game-btn'>
-                <Button>게임시작</Button>
-                <Button>도움말</Button>
+                <Button variant="light" className='game-btn-1' onClick={ handleShow }>게임시작</Button>
+                <Button variant="light" className='game-btn-2'>도움말</Button>
             </div>
             <div className="game-main-bg-2" style={{ backgroundImage: `url(${일반쓰레기통})` }} />
         </div>
+
+        <Modal show={showModal} onHide={handleClose} className='modal-design'>
+            <Modal.Header closeButton>
+            {/* <Modal.Title>분리수거 게임</Modal.Title> */}
+            </Modal.Header>
+            <Modal.Body>
+            <p>분리수거 타임어택 게임 시작 ! <br/> 제한시간은 1분이고 게임은 바로 시작됩니다. </p>
+            </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>안 할래요</Button>
+            <Button variant="success" onClick={()=>{
+                navigate('/game');
+            }}>바로 시작할게요</Button>
+            </Modal.Footer>
+        </Modal>
+
         </>
     );
 }

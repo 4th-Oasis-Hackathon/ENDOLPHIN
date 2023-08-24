efd8888189950bef87e03c04a1ac04297da3719c
yunyoung0531 <yunyoung0531@naver.com>
2023-08-24 21:17:26 +0900

    검색 기능 추가 및 이미지 정렬

---
+++ endolphin/src/data.js
@@ -0,0 +1,215 @@
+let data =
+[
+    {
+        "id": 0,
+        "name": "종이",
+        "instructions": "종이는 재활용통에 넣으세.",
+        "image": require("./pages/imgs/종이박스쓰레기.png"),
+        "img-inst": "종이박스"
+    },
+    {
+        id: 1,
+        name: "플라스틱",
+        instructions: "플라스틱s은 ~d",
+        image: require("./pages/imgs/색깔플라스틱병쓰레기.png"),
+        "img-inst": "색깔플라스틱병"
+    },
+    {
+        id: 1,
+        name: "색깔 플라스틱병",
+        instructions: "플라스틱s은 ~d",
+        image: require("./pages/imgs/색깔플라스틱병쓰레기.png"),
+        "img-inst": "색깔플라스틱병"
+    },
+    {
+        "id": 2,
+        "name": "캔",
+        "instructions": "캔은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/캔쓰레기.png"),
+        "img-inst": "캔 · 고철류"
+    },
+    {
+        "id": 2,
+        "name": "고철류",
+        "instructions": "캔은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/캔쓰레기.png"),
+        "img-inst": "캔 · 고철류"
+    },
+    {
+        "id": 3,
+        "name": "멸균팩",
+        "instructions": "멸균팩은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/종이팩쓰레기.png"),
+        "img-inst": "종이팩"
+    },
+    {
+        "id": 4,
+        "name": "과자봉지",
+        "instructions": "과자봉지는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/과자봉지쓰레기.png"),
+        "img-inst": "과자봉지"
+    },
+    {
+        "id": 4,
+        "name": "과자봉투",
+        "instructions": "과자봉지는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/과자봉지쓰레기.png"),
+        "img-inst": "과자봉지"
+    },
+    {
+        "id": 5,
+        "name": "깨진 유리",
+        "instructions": "깨진 유리는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/깨진유리쓰레기.png"),
+        "img-inst": "깨진유리"
+    },
+    {
+        "id": 5,
+        "name": "깨진유리",
+        "instructions": "깨진 유리는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/깨진유리쓰레기.png"),
+        "img-inst": "깨진유리"
+    },
+    {
+        "id": 6,
+        "name": "종이팩",
+        "instructions": "종이팩은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/종이팩쓰레기.png"),
+        "img-inst": "종이팩"
+    },
+    {
+        "id": 7,
+        "name": "병뚜껑",
+        "instructions": "병뚜껑은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/병뚜껑쓰레기.png"),
+        "img-inst": "병뚜껑"
+    },
+    {
+        "id": 8,
+        "name": "비닐봉지",
+        "instructions": "비닐봉지는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/비닐봉지쓰레기.png"),
+        "img-inst": "비닐봉지"
+    },
+    {
+        "id": 8,
+        "name": "비닐봉투",
+        "instructions": "비닐봉지는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/비닐봉지쓰레기.png"),
+        "img-inst": "비닐봉지"
+    },
+    {
+        "id": 9,
+        "name": "스티로폼",
+        "instructions": "스티로폼은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/스티로폼쓰레기.png"),
+        "img-inst": "스티로폼"
+    },
+    {
+        "id": 9,
+        "name": "스타이로폼",
+        "instructions": "스티로폼은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/스티로폼쓰레기.png"),
+        "img-inst": "스티로폼"
+    },
+    {
+        "id": 10,
+        "name": "스프레이용기",
+        "instructions": "스프레이용기는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/스프레이용기쓰레기.png"),
+        "img-inst": "스프레이용기"
+    },
+    {
+        "id": 10,
+        "name": "스프레이",
+        "instructions": "스프레이용기는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/스프레이용기쓰레기.png"),
+        "img-inst": "스프레이 용기"
+    },
+    {
+        "id": 11,
+        "name": "유리병",
+        "instructions": "유리병은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/유리병쓰레기.png"),
+        "img-inst": "유리병"
+    },
+    {
+        "id": 11,
+        "name": "유리",
+        "instructions": "유리병은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/유리병쓰레기.png"),
+        "img-inst": "유리병"
+    },
+    {
+        "id": 12,
+        "name": "종이박스",
+        "instructions": "종이박스는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/종이박스쓰레기.png"),
+        "img-inst": "종이박스"
+    },
+    {
+        "id": 12,
+        "name": "종이 박스",
+        "instructions": "종이박스는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/종이박스쓰레기.png"),
+        "img-inst": "종이박스"
+    },
+    {
+        "id": 13,
+        "name": "폐건전지",
+        "instructions": "폐건전지는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/폐건전지쓰레기.png"),
+        "img-inst": "폐건전지"
+    },
+    {
+        "id": 13,
+        "name": "건전지",
+        "instructions": "폐건전지는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/폐건전지쓰레기.png"),
+        "img-inst": "폐건전지"
+    },
+    {
+        "id": 14,
+        "name": "플라스틱병",
+        "instructions": "플라스틱병은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/플라스틱병쓰레기.png"),
+        "img-inst": "플라스틱병"
+    },
+    {
+        "id": 15,
+        "name": "플라스틱 빨대",
+        "instructions": "플라스틱 빨대는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/플라스틱빨대쓰레기.png"),
+        "img-inst": "플라스틱 빨대"
+    },
+    {
+        "id": 15,
+        "name": "플라스틱빨대",
+        "instructions": "플라스틱 빨대는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/플라스틱빨대쓰레기.png"),
+        "img-inst": "플라스틱 빨대"
+    },
+    {
+        "id": 15,
+        "name": "빨대",
+        "instructions": "플라스틱 빨대는 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/플라스틱빨대쓰레기.png"),
+        "img-inst": "플라스틱 빨대"
+    },
+    {
+        "id": 6,
+        "name": "종이팩",
+        "instructions": "종이팩은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/종이팩쓰레기.png"),
+        "img-inst": "종이팩"
+    },
+    {
+        "id": 6,
+        "name": "종이팩",
+        "instructions": "종이팩은 재활용통에 넣으세요.",
+        "image": require("./pages/imgs/종이팩쓰레기.png"),
+        "img-inst": "종이팩"
+    },
+]
+
+export default data;
